bbf87d249f8d4482eeead0a5f8a0b9ff9b091f14
Michael Morris <michaelmorrismm@gmail.com>
2019-12-22 14:36:11 -0700

    Add Spring Initializr seed

---
+++ src/main/java/com/michaelmorris/gametracker/GameTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.michaelmorris.gametracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelmorris/gametracker/GameTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelmorris.gametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
